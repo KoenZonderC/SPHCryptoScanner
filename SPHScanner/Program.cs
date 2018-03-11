fc99bc57d5ceebd4b54ce66265fc4c127e73faec
Erwin Beckers <erwin@mbp-van-mpw.lan>
2018-03-11 11:21:20 +0100

    scan binance and bittrex for sphs

--- SPHScanner/Program.cs
+++ SPHScanner/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,35 +15,48 @@ namespace SPHScanner
         {
             Console.WriteLine("SPH Scanner 1.0 (c) 2018 Erwin Beckers");
             Console.WriteLine("");
-            Console.WriteLine("Loading symbols from bittrex");
 
-           Task.Run(async () =>
-           {
-               var strategy = new SPHStrategy();
-               var api = new ExchangeBittrexAPI();
-               var symbols = (await api.GetSymbolsAsync()).Where(e => e.StartsWith("BTC-")).OrderBy(e=>e).ToList();
-               Console.WriteLine($"got {symbols.Count} btc pairs from bittrex");
-               Console.WriteLine("scanning all btc pairs for SPH's....");
+            ScanExchange("bittrex", new ExchangeBittrexAPI()); 
 
-               var idx = 1;
-               foreach (var symbol in symbols)
-                {
-                   Debug.WriteLine($"{idx}/{symbols.Count} scanning {symbol}");
-                   idx++;
-                     
-                    var candles = await api.GetCandlesAsync(symbol, TIMEFRAME_H1, DateTime.Now.AddMonths(-1));
-                   var sphs = strategy.Find(symbol, candles.ToList());
-                   foreach(var sph in sphs)
-                    {
-                        Console.WriteLine($"SPH: {sph.Date}   {symbol}   {sph.Price}");    
-                    }
-               }
-           })
-           .Wait();
+            ScanExchange("binance", new ExchangeBinanceAPI()); 
 
 
             Console.WriteLine("--- done ---");
             Console.ReadLine();
+        }
+
+        private static void ScanExchange(string exchange, ExchangeAPI api)
+        {
+            Console.WriteLine($"Loading symbols from {exchange}");
+            Task.Run(async () =>
+            {
+                var strategy = new SPHStrategy();
+                var allSymbols = (await api.GetSymbolsAsync()).OrderBy(e => e).ToList();
+                var symbols = new List<string>();
+                foreach(var symbol in allSymbols)
+                {
+                    if (symbol.StartsWith("BTC-") || symbol.EndsWith("BTC")) symbols.Add(symbol);
+                }
+                Console.WriteLine($"got {symbols.Count} btc pairs from {exchange}");
+                Console.WriteLine("scanning all btc pairs for SPH's....");
+
+                var idx = 1;
+                foreach (var symbol in symbols)
+                {
+                    Debug.WriteLine($"{idx}/{symbols.Count} scanning {symbol}");
+                    idx++;
+
+                    var candles = (await api.GetCandlesAsync(symbol, TIMEFRAME_H1, DateTime.Now.AddMonths(-1))).ToList();
+                    var sphs = strategy.Find(symbol, candles);
+                    foreach (var sph in sphs)
+                    {
+                        Console.WriteLine($"SPH: {sph.Date}   {symbol}   {sph.Price}");
+                    }
+                }
+            })
+            .Wait();
+            Console.WriteLine("");
+            Console.WriteLine("");
         }
     }
 }
