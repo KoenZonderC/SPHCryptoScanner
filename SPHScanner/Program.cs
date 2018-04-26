e88d984a9b6de039ca575270d61daeef89f50efb
Beckers, E. (Erwin) <info@erwinbeckers.nl>
2018-04-26 12:30:10 +0200

    fixed bitfinex scanning

--- SPHScanner/Program.cs
+++ SPHScanner/Program.cs
@@ -14,11 +14,7 @@ namespace SPHScanner
 
         static void Main(string[] args)
         {
-            //SPH: 2018-03-18 17:00:00          BTC-CLAM panic:05.10% in 1 hours, stability:  2 hours, recovery:1 hours,  price: 0.00039249
-            //SPH: 2018-03-18 16:00:00          BTC-DOGE panic:05.13% in 1 hours, stability:  3 hours, recovery:2 hours,  price: 0.00000037
-            //SPH: 2018-03-15 06:00:00          BTC-PDC panic:07.74% in 1 hours, stability:  2 hours, recovery:2 hours,  price: 0.00000286
-            //SPH: 2018-02-06 08:00:00          NULSBTC panic:09.36 % in 1 hours, stability: 2 hours, recovery: 2 hours,  price: 0.00022659
-            Console.WriteLine("SPH Scanner 1.04 (c) 2018 Erwin Beckers");
+            Console.WriteLine("SPH Scanner 1.05 (c) 2018 Erwin Beckers");
             Console.WriteLine("");
 
             // create database if it doesnt exists yet
@@ -29,8 +25,12 @@ namespace SPHScanner
                 // create strategy
                 var strategy = new SPHStrategy();
 
+                // scan SPH's on bitfinex
+                var scanner = new Scanner(db, ExchangeTypes.Bitfinex);
+                scanner.Scan(strategy);
+
                 // scan SPH's on kraken
-                var scanner = new Scanner(db, ExchangeTypes.Kraken);
+                scanner = new Scanner(db, ExchangeTypes.Kraken);
                 scanner.Scan(strategy);
 
                 // scan SPH's on bittrex
